988df631e0d45debeeb159f98506d29bf0a80b2e
AwangDarmawan <adarmawan468@gmail.com>
2026-06-18 10:40:22 +0700

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 import Intro from "./Pages/Intro";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 
 
 
