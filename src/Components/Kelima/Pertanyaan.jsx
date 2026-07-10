3e95956fdfde1b1dfe42712f3efde9374b1a5f80
AwangDarmawan <adarmawan468@gmail.com>
2026-07-10 20:57:43 +0700

    push

--- src/Components/Kelima/Pertanyaan.jsx
+++ src/Components/Kelima/Pertanyaan.jsx
@@ -29,10 +29,15 @@ const Pertanyaan = [
     answer:
       "Ya, undangan digital dapat dibagikan melalui melalui WhatsApp, Facebook, Twitter, atau salin teks undangan. Untuk Facebook Lite dan instagram, gunakan fitur salin teks karena berbagi langsung belum didukung",
   },
+   {
+    question: "Apakah bisa pilih musik?",
+    answer:
+      "Ya, kami untuk musik bisa request",
+  },
   {
     question: "Apakah bisa custom tamu",
     answer:
-      "Ya, undangan digital dapat custom nama tamu unlimited tak terbatas yang akan diundang melalui fitur custom nama agar setiap tamu mendapatkan undangan yang lebih personal",
+      "Ya, undangan digital dapat custom nama tamu unlimited TAK TERBATAS yang akan diundang melalui fitur custom nama agar setiap tamu mendapatkan undangan yang lebih personal",
   },
 ];
 export default Pertanyaan
